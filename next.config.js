2939afa9f13542d1cf4a52375cfa7b63a154b313
monty-src <phoebe@montiers-mbp.lan>
2023-03-13 17:50:50 -0400

    Dev added nextJs config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,31 @@
+/**
+ * @module next.config
+ *
+ *
+ * @author montier.elliott@gmail.com
+ * @description NextJS configuration
+ */
 /** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-}
+
+/**
+ * Configuration object for NextJS application.
+ * 
+ * 
+ * @typedef {Object} NextConfig
+ * @property {boolean} reactStrictMode - React strict mode.
+ * @property {Object} images - NextJS images.
+ * @property {string[]} images.domains - Allowed domains for NextJS images.
+ */
+
+/**
+ * Configuration object for the current NextJS application.
+ * @type {NextConfig}
+ */
+const nextConfig = {
+  reactStrictMode: false,
+  images: {
+    domains: ["links.papareact.com", "image.tmdb.org"],
+  },
+};
+
+module.exports = nextConfig;
