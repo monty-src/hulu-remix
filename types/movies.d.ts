c347e9db373af00529c0e1fc4254a0ac862dff40
monty-src <phoebe@montiers-mbp.lan>
2023-03-16 22:48:40 -0400

    Updated type comments

---
+++ types/movies.d.ts
@@ -0,0 +1,20 @@
+/**
+ * @module types/movies.d
+ *
+ *
+ * @author montier.elliott@gmail.com
+ */
+
+/**
+ * Movie information
+ * 
+ * 
+ * @interface
+ * @typedef {Object} iMovie
+ * @property {string} url
+ * @property {string} title
+ */
+export interface iMovie {
+  url: string;
+  title: string;
+}
--- types/results.d.ts
+++ types/results.d.ts
@@ -1,28 +1,38 @@
 /**
- * @typedef {Object} Result
- * @property {number} id - The ID of the result.
- * @property {string} backdrop_path - The path of the backdrop image.
- * @property {string} poster_path - The path of the poster image.
- * @property {string} overview - The overview of the result.
- * @property {string=} title - The title of the result.
- * @property {string=} original_name - The original name of the result.
- * @property {string=} media_type - The media type of the result.
- * @property {string=} release_date - The release date of the result.
- * @property {string=} first_air_date - The first air date of the result.
- * @property {number} vote_count - The vote count of the result.
+ * @module types/results.d
  *
- * @typedef {Object} Props
- * @property {Result[]} results - An array of results to display
+ *
+ * @author montier.elliott@gmail.com
+ */
+
+/**
+ * Movie results
+ * 
+ * 
+ * @interface
+ * @typedec {Object} iResults
+ * @property {number} id
+ * @property {string} backdrop_path
+ * @property {string} poster_path
+ * @property {string} overview
+ * @property {string} title
+ * @property {string} original_name
+ * @property {string} media_type
+ * @property {string} release_date
+ * @property {string} first_air_date
+ * @property {number} vote_count
  */
 export interface iResults {
   id: number;
-  backdrop_path: string;
-  poster_path: string;
   overview: string;
+  vote_count: number;
+  poster_path: string;
+  backdrop_path: string;
+
   title?: string;
-  original_name?: string;
   media_type?: string;
   release_date?: string;
+  original_name?: string;
   first_air_date?: string;
-  vote_count: number;
+  
 }
